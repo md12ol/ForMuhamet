9c8cd1038557367a43f9906341d1145464b32469
Michael Dubé <michael.dube@ovgu.de>
2025-10-29 17:44:39 +0100

    Added task for Muhamet.

---
+++ Combo_test.cpp
@@ -0,0 +1,21 @@
+#include "Graph/Graph.h"
+#include "SDA/SDA.h"
+
+int main() {
+    /*
+     * TODO: See if you can create an SDA and use its output to create a graph.
+     *
+     * Consider a graph that is stored in an upper-triangular adjacency matrix
+     * That means if a graph has 4 nodes: A, B, C, and D with the following connections
+     * A <-> B
+     * B <-> C
+     * B <-> D
+     * Then the matrix would look like this:
+     * 0 1 0 0
+     * 0 0 1 1
+     * 0 0 0 0
+     * 0 0 0 0
+     *
+     * In general, a graph with N nodes needs N*(N-1)/2 weights to store all the possible edges.
+    */
+}
